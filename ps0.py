6bf862f1bc6e41916f8a3e58d801f15d97904967
Nkwawir Beltus W <nkwawirbeltus2222@gmail.com>
2019-02-10 20:41:55 +0300

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 30 11:05:43 2019
+
+@author: Acer
+"""
+import numpy as np
+
+x = int(input("Enter a number X:"));
+y = int(input("Enter a number Y:"));
+
+print("x ** y =", np.power(x,y));
+print("log(X)= ",np.log2(x));
+
+ 
+ 
